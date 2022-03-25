076b62728888c8e26ba8a74fb6bb503855b41062
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2022-03-24 23:35:01 -0400

    plusMinus function almost finished

--- hackerrank-sandbox.js
+++ hackerrank-sandbox.js
@@ -98,3 +98,41 @@ function getUniqueCharacter(s) {
       return valid.sort();
   }
   
+
+//   Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
+
+function plusMinus(arr) {
+    // .split SPLITS a string into an array of substrings
+    input = input.split("\n");
+    // the shift() method removes the first element from an array and returns that removed element
+    // this method changes the length of the array.
+    var n = input.shift();
+    input = input.shift().split(' ');
+    var len = input.length;
+    
+    var neg = 0.0;
+    var zero = 0.0;
+    var pos = 0.0;
+    
+    input.forEach(function (num) {
+        num = parseInt(num);
+        if (num < 0) { neg++ }
+        else if (num > 0) { pos++ }
+        else { zero++ }
+    });
+
+    console.log((pos / len).toPrecision(3));
+    console.log((neg / len).toPrecision(3));
+    console.log((zero / len).toPrecision(3));
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+    processData(_input);
+ });
